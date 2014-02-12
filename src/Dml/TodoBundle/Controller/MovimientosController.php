e7e7f9e4b3ff4bbd9f58b7dae29afdb023ad05aa
Oswaldo <Oswaldo@VGN-FW280FY>
2014-02-11 23:12:49 -0500

    Mejorando consultas en el controlador

--- src/Dml/TodoBundle/Controller/MovimientosController.php
+++ src/Dml/TodoBundle/Controller/MovimientosController.php
@@ -14,22 +14,25 @@ class MovimientosController extends Controller {
     
     public function indexAction(Request $request) {
         $em = $this->getDoctrine();
-        if ($request->get('id') != NULL):
-            $q = $em->getRepository('TodoBundle:Movimientos')->moTable()
-                    ->addSelect('ah')
-                    ->join('mo.ahorrosAh', 'ah')
-                    ->where('ah.ahId = :id')
-                    ->addOrderBy('mo.moFecha','desc')
-                    ->setParameter('id', $request->get('id'));
-        else:
-            $q = $em->getRepository('TodoBundle:Movimientos')->moTable()
-                    ->addSelect('ah')
-                    ->join('mo.ahorrosAh', 'ah')
-                    ->where('ah.ahId = :id')
-                    ->addOrderBy('mo.moFecha','desc')
-                    ->setParameter('id', 1);
-        endif;
-        $q = $q->getQuery()->getArrayResult();
+        $fields = array(
+                        'mo.moId',
+                        'mo.moFecha',
+                        'mo.moConcepto',
+                        'mo.moTipo',
+                        'mo.moDocumento',
+                        'mo.moOficina',
+                        'mo.moMonto',
+                        'mo.moSaldo',
+                        'ah.ahNumeroCuenta'
+                       );
+        $q = $em->getRepository('TodoBundle:Movimientos')->moTable()
+                ->select($fields)
+                ->join('mo.ahorrosAh', 'ah')
+                ->where('ah.ahId = :id')
+                ->addOrderBy('mo.moFecha','desc')
+                ->setParameter('id', $request->get('id') != NULL ? $request->get('id') : 1)
+                ->getQuery()
+                ->getArrayResult();
 //        Util::getMyDump($q);
         return $this->render('TodoBundle:Movimientos:index.html.twig', array('movimientos' => $q));
     }
@@ -37,8 +40,13 @@ class MovimientosController extends Controller {
     public function cuentasAhorrosAction(Request $request) {
         $em = $this->getDoctrine();
         $q = $em->getRepository('TodoBundle:Ahorros')->ahTable()
+                ->select('ah.ahId, ah.ahNumeroCuenta')
                 ->getQuery()
                 ->getArrayResult();
+//        echo '<textarea rows="4" cols="100">';
+//        print_r($q->getSQL());
+//        echo '</textarea>';
+//        die();
 //        Util::getMyDump($q);
         return new Response(Util::getJSON($q));
     }
