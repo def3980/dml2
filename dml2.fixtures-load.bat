58835b105125352f10f12e7b5759fbf5d2f4d5af
Oswaldo <Oswaldo@VGN-FW280FY>
2014-03-02 13:08:32 -0500

    Actualizando batch para la recarga de datos

--- dml2.fixtures-load.bat
+++ dml2.fixtures-load.bat
@@ -1,4 +1,13 @@
-c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:database:drop --force
-c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:database:create
-c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:schema:create
-c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:fixtures:load --no-interaction
+::c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:database:drop --force
+::c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:database:create
+::c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:schema:create
+::c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:fixtures:load --no-interaction
+@echo off
+set ruta=%cd%
+set exec="\app\console"
+set espacio= 
+echo.
+php %ruta%%exec%%espacio%doctrine:database:drop --force
+php %ruta%%exec%%espacio%doctrine:database:create
+php %ruta%%exec%%espacio%doctrine:schema:create
+php %ruta%%exec%%espacio%doctrine:fixtures:load --no-interaction
