e59fb7972f7a289151dfd04138eef762180e58b1
Oswaldo <Oswaldo@10.0.2.15>
2014-03-20 17:03:07 -0500

    Ajax en el paginador y combo box

--- src/Dml/TodoBundle/Controller/MovimientosController.php
+++ src/Dml/TodoBundle/Controller/MovimientosController.php
@@ -12,39 +12,62 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MovimientosController extends Controller {
     
-    public function indexAction(Request $request) {
-        $em = $this->getDoctrine();
+    protected 
         $fields = array(
-                        'mo.moId',
-                        'mo.moFecha',
-                        'mo.moConcepto',
-                        'mo.moTipo',
-                        'mo.moDocumento',
-                        'mo.moOficina',
-                        'mo.moMonto',
-                        'mo.moSaldo',
-                        'ah.ahNumeroCuenta'
-                       );
+                    'mo.moId',
+                    'mo.moFecha',
+                    'mo.moConcepto',
+                    'mo.moTipo',
+                    'mo.moDocumento',
+                    'mo.moOficina',
+                    'mo.moMonto',
+                    'mo.moSaldo',
+                    'ah.ahNumeroCuenta'
+                   );
+
+    private function listaDMovimientos(Request $request) {
+        $em = $this->getDoctrine();
         $repo = $em->getRepository('TodoBundle:Movimientos')
                    ->moTable()
-                   ->select($fields)
+                   ->select($this->fields)
                    ->join('mo.ahorrosAh', 'ah')
                    ->andWhere('ah.ahId = :id')
                    ->setParameter('id', $request->get('id') != NULL ? $request->get('id') : 1);
-        $this->pager = new Paginador('TodoBundle:Movimientos', 5);
+        $this->pager = new Paginador('TodoBundle:Movimientos', 7);
         $this->pager->setSelect($repo);
         $this->pager->setPage($request->get('page') != NULL ? $request->get('page') : 1);
         $this->pager->init();
-        return $this->render('TodoBundle:Movimientos:index.html.twig', array('pager' => $this->pager));
+    }
+
+    private function cuentasDAhorros(Request $request) {
+        $em = $this->getDoctrine();
+        return $em->getRepository('TodoBundle:Ahorros')->ahTable()
+                  ->select('ah.ahId, ah.ahNumeroCuenta');
+    }
+
+    public function indexAction(Request $request) {
+        $this->listaDMovimientos($request);
+        $ahorros = $this->cuentasDAhorros($request)->getQuery()->getArrayResult();
+        return $this->render('TodoBundle:Movimientos:index.html.twig', array('pager' => $this->pager, 'ahorros' => $ahorros));
     }
     
-    public function cuentasAhorrosAction(Request $request) {
-        $em = $this->getDoctrine();
-        $q = $em->getRepository('TodoBundle:Ahorros')->ahTable()
-                ->select('ah.ahId, ah.ahNumeroCuenta')
-                ->getQuery()
-                ->getArrayResult();
-        return new Response(Util::getJSON($q));
+//    public function cuentasAhorrosAction(Request $request) {
+//        $em = $this->getDoctrine();
+//        $q = $em->getRepository('TodoBundle:Ahorros')->ahTable()
+//                ->select('ah.ahId, ah.ahNumeroCuenta')
+//                ->getQuery()
+//                ->getArrayResult();
+//        return new Response(Util::getJSON($q));
+//    }
+    
+    public function moListAction(Request $request) {
+        $this->listaDMovimientos($request);
+        return $this->render('TodoBundle:Movimientos:mo_list.html.twig', array('pager' => $this->pager));
+    }
+    
+    public function moPagerAction(Request $request) {
+        $this->listaDMovimientos($request);
+        return $this->render('TodoBundle:Movimientos:mo_pager.html.twig', array('pager' => $this->pager));
     }
     
 }
