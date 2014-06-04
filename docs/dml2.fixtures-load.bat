84243214b3594f7b80e48fa3429a42cd08e499f5
uno <uno@dos.com>
2014-06-04 15:49:56 -0500

    Actualizando manejador de recargas de BDD

--- docs/dml2.fixtures-load.bat
+++ docs/dml2.fixtures-load.bat
@@ -1,12 +1,10 @@
-::c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:database:drop --force
-::c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:database:create
-::c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:schema:create
-::c:\xampp\php\php.exe c:\xampp\htdocs\dml2\app\console doctrine:fixtures:load --no-interaction
 @echo off
-set ruta=%cd ..%
-set exec="\app\console"
+
+set exec="app\console"
 echo.
-php %ruta%%exec% doctrine:database:drop --force
-php %ruta%%exec% doctrine:database:create
-php %ruta%%exec% doctrine:schema:create
-php %ruta%%exec% doctrine:fixtures:load --no-interaction
+
+cd ..
+php %exec% doctrine:database:drop --force
+php %exec% doctrine:database:create
+php %exec% doctrine:schema:create
+php %exec% doctrine:fixtures:load --no-interaction
